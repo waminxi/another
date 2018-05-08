5e0c85c4d90e064c0749b3096e703affd3a339d0
unknown <wangmingxing@wangmingxing.hongshu.com>
2018-05-08 18:05:31 +0800

    understand some rules on express4.0 development about router

--- public/javascripts/busi/source.js
+++
@@ -1,9 +0,0 @@
-var dbconnect = require(["./db"],function(){
-    alert("load finished");
-});
-
-function connect() {
-    var mongoIp = $('#address').val();
-    var mongoPort = $('#port').val();
-    connect(mongoIp, mongoPort, 'cdn');
-}
--- routes/source.js
+++ routes/source.js
@@ -1,10 +1,20 @@
-var express = require('express');
+var express = require('express'),
+    users = require('../models/users');
 var router = express.Router();
 
-router.get('/',function(req, res){
-    res.render('source',{
-        title:'导出数据到json文件'
+router.get('/', function (req, res) {
+    res.render('source', {
+        title: '导出数据到json文件'
     })
+});
+
+router.post('/', function (req, res) {
+    // console.log('post');
+    console.log(req.body['address']);
+    users.get("123",function(err){
+        console.log(err);
+    })
+    // res.redirect('/source');
 });
 
 module.exports = router;
