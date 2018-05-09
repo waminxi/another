625a33244be7fde3cbc66ced7f8a3c92e61869ec
unknown <wangmingxing@hongshu.com>
2018-05-09 18:32:03 +0800

    before debug ajax

--- public/javascripts/busi/source.js
+++ public/javascripts/busi/source.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
+    $("#loading").hide();
     $("#submit").on("click", function (event) {
-        $("#loading").hide();
+        $("#loading").show();
+        var params = {
+            address: $("#address").val(),
+            port: $("#port").val(),
+            dbName: $("#dbName").val(),
+        };
+        $.ajax({
+            data: params,
+            url: '/source',
+            method: 'post',
+            dataType: 'json',// 此参数不确定是否有用
+            cache: 'false',
+            timeout: 5000,
+            success: function (data) {
+                var data = $.parseJSON(data);
+                $("#loading").hide();
+                alert(data.message);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#loading").hide();
+                alert('error ' + textStatus + " " + errorThrown);
+            }
+        })
     });
 });
